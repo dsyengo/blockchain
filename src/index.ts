aa10dc56d35f63f7e9934badae5f8231f1ba5905
dsyengo <deniswilson028@gmail.com>
2023-11-10 12:13:16 +0300

    commit-1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,94 @@
 // cannister code goes here
+
+// Import the required libraries
+import { 
+    Actor, 
+    HttpAgent 
+  } from '@dfinity/agent';
+import { EmptyClass, ReservedClass, UnknownClass, BoolClass, NullClass, TextClass, IntClass, NatClass, FloatClass, FixedIntClass, FixedNatClass, PrincipalClass, TupleClass, Type, VecClass, OptClass, RecordClass, VariantClass, RecClass, FuncClass, ServiceClass } from '@dfinity/candid/lib/cjs/idl';
+  
+  // Initialize the agent
+  const agent = new HttpAgent();
+  const actor = Actor.createActor(canisterId, {
+      agent,
+      canisterId: ''
+  });
+  // Define the message structure
+  type Message = {
+    id: string;
+    title: string;
+    body: string;
+    attachmentURL: string;
+    createdAt: bigint;
+    updatedAt?: bigint;
+  };
+  
+  // Define the message payload structure
+  type MessagePayload = {
+    title: string;
+    body: string;
+    attachmentURL: string;
+  };
+  
+  // Store messages in a Map
+  const messageStorage = new Map<string, Message>();
+  
+  // Get all messages
+  export async function getMessages(): Promise<Message[]> {
+    return Array.from(messageStorage.values());
+  }
+  
+  // Get a specific message by ID
+  export async function getMessage(id: string): Promise<Message | undefined> {
+    return messageStorage.get(id);
+  }
+  
+  // Add a new message
+  export async function addMessage(payload: MessagePayload): Promise<Message> {
+    const message: Message = {
+      id: uuidv4(),
+      createdAt: BigInt(Date.now()),
+      ...payload
+    };
+    messageStorage.set(message.id, message);
+    return message;
+  }
+  
+  // Update an existing message
+  export async function updateMessage(id: string, payload: MessagePayload): Promise<Message | undefined> {
+    const existingMessage = messageStorage.get(id);
+    if (existingMessage) {
+      const updatedMessage: Message = {
+        ...existingMessage,
+        ...payload,
+        updatedAt: BigInt(Date.now())
+      };
+      messageStorage.set(id, updatedMessage);
+      return updatedMessage;
+    }
+    return undefined;
+  }
+  
+  // Delete a message by ID
+  export async function deleteMessage(id: string): Promise<Message | undefined> {
+    const existingMessage = messageStorage.get(id);
+    if (existingMessage) {
+      messageStorage.delete(id);
+      return existingMessage;
+    }
+    return undefined;
+  }
+  
+  // Generate a random UUID
+  function uuidv4(): string {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      const r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  }
+
+function canisterId(idl: { IDL: { Empty: EmptyClass; Reserved: ReservedClass; Unknown: UnknownClass; Bool: BoolClass; Null: NullClass; Text: TextClass; Int: IntClass; Nat: NatClass; Float32: FloatClass; Float64: FloatClass; Int8: FixedIntClass; Int16: FixedIntClass; Int32: FixedIntClass; Int64: FixedIntClass; Nat8: FixedNatClass; Nat16: FixedNatClass; Nat32: FixedNatClass; Nat64: FixedNatClass; Principal: PrincipalClass; Tuple: <T extends any[]>(...types: T) => TupleClass<T>; Vec: <T>(t: Type<T>) => VecClass<T>; Opt: <T>(t: Type<T>) => OptClass<T>; Record: (t: Record<string, Type<any>>) => RecordClass; Variant: (fields: Record<string, Type<any>>) => VariantClass; Rec: () => RecClass<any>; Func: (args: Type<any>[], ret: Type<any>[], annotations?: string[] | undefined) => FuncClass; Service(t: Record<string, FuncClass>): ServiceClass; }; }): ServiceClass {
+    throw new Error('Function not implemented.');
+}
+
